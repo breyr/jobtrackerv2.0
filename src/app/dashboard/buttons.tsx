5d704b740094447d212727ff1e337b69ef24f708
breyr <breyr2021@gmail.com>
2023-12-18 20:53:22 -0500

    changed variant of search input

--- src/app/dashboard/buttons.tsx
+++ src/app/dashboard/buttons.tsx
@@ -6,7 +6,13 @@ export default function ButtonRow({ userId }: { userId: string }) {
   return (
     <div className="flex flex-row gap-4 w-11/12 items-center py-4 mx-auto pl-6">
       <NewApplication userId={userId} />
-      <Input type="search" label="Search" size="sm" className="w-1/5" />
+      <Input
+        type="search"
+        variant="bordered"
+        label="Search"
+        size="sm"
+        className="w-1/5"
+      />
     </div>
   );
 }
