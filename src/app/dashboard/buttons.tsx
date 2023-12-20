12cafc4577634b0d9d9504eff477e9720a4eb191
breyr <breyr2021@gmail.com>
2023-12-20 12:42:21 -0500

    ui changes to button row

--- src/app/dashboard/buttons.tsx
+++ src/app/dashboard/buttons.tsx
@@ -5,11 +5,11 @@ import Search from "@/components/Search";
 
 export default function ButtonRow({ userId }: { userId: string }) {
   return (
-    <div className="flex flex-row gap-4 w-11/12 justify-between py-4 mx-auto pl-6 items-center">
+    <div className="flex flex-row gap-4 w-11/12 justify-between py-4 mx-auto items-center ">
       <div>
         <NewApplication userId={userId} />
       </div>
-      <div className="flex items-center gap-3 w-2/5">
+      <div className="flex items-center justify-end gap-3 w-2/5 ">
         <FilterModal />
         <Search />
       </div>
